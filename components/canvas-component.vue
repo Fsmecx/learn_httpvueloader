8927711d0ad561c73a6a45446d74cad50dc1980e
Leyu ZHang <31591357+leyuz@users.noreply.github.com>
2019-02-03 23:17:55 +0800

    test

---
+++ components/canvas-component.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Canvas</h1>
+    <pixel-component/>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  name: "canvas-component",
+  compoents: {
+    "pixel-component": httpVueLoader("components/pixel-component.vue")
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ components/pixel-component.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>this is pixel</div>
+</template>
+<script>
+module.exports = {
+  name: "pixel-component"
+};
+</script>
+
+<style>
+</style>
